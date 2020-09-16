d7a682927bd09e268fdf0603af90a4b9c97e785f
Zvonimir <44640961+Zvonimirko@users.noreply.github.com>
2020-09-16 11:54:14 +0200

    Implementing CoffeeCard into Content

--- material-react/src/Content.jsx
+++ material-react/src/Content.jsx
@@ -1,10 +1,20 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
+import CoffeeCard from "./CoffeeCard";
 
 function Content() {
   return (
-    <div>
-      <h1>This is our content</h1>
-    </div>
+    <Grid container spacing={4}>
+      <Grid item xs={6} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      {/* <Grid item xs={6} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={6} sm={4}>
+        <CoffeeCard />
+      </Grid> */}
+    </Grid>
   );
 }
 
